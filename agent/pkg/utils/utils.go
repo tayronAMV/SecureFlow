67208378e1e984c855776d1f61b78a9aec4e614f
tayronAMV <amitay2005@gmail.com>
2025-05-09 11:34:07 +0300

    fixed calc of syscall,network over interval

--- agent/pkg/utils/utils.go
+++ agent/pkg/utils/utils.go
@@ -5,6 +5,7 @@ import (
 	"agent/pkg/logs"
 )
 
+const TIME_INTERVAL = float64(10.0)
 
 var Container_uid_map = make(map[string]*logs.Anomaly_log)
 
@@ -14,6 +15,14 @@ var MemoryTrackers = make(map[string]logs.MemoryTracker)
 
 func Send_to_Server_Reset(){
 	for _ , ptr := range Container_uid_map{
+		count_syscalls  := ptr.Syscall
+		total_network := ptr.Network
+
+
+		ptr.Syscall = count_syscalls / TIME_INTERVAL // avg over interval , syscall per second for this container
+		ptr.Network = total_network / TIME_INTERVAL
+
+
 		logs.Producer(*ptr)
 	}
 	
